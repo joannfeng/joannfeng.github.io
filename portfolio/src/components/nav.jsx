8ce08c724b5366c8090ef77b2aa937579244f3a2
Joann Feng <joannf310@gmail.com>
2025-05-25 15:01:33 -0500

    refine navbar responsiveness

--- portfolio/src/components/nav.jsx
+++ portfolio/src/components/nav.jsx
@@ -1,19 +1,57 @@
 import {CgNametag} from 'react-icons/cg';
+import {AiOutlineClose} from 'react-icons/ai';
+import {HiMenuAlt1} from 'react-icons/hi';
+import {useState} from 'react';
 
 const nav = () => {
+
+const [toggle, setToggle] = useState(false);
+
+function openMenu(){
+    setToggle(true);
+}
+function closeMenu(){
+    setToggle(false);
+}
+
     return (
         <>
-            <div className="flex items-center justify-between p-10 lg:flex-row text-indigo-600">
+            <div className="flex items-center justify-between p-10 md:flex-row text-indigo-600">
                 <div>
-                <a href="#" className="text-white font-mono text-3xl trarcking-wider flex items-center"><CgNametag/> Joann Feng</a>
+                    <a href="#" className="text-white font-mono text-3xl tracking-wider flex items-center"><CgNametag/> Joann Feng</a>
                 </div>
                 <div className="space-x-4">
-                    <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Home</a>
-                    <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">About Me</a>
-                    <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Resume</a>
-                    <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Projects</a>
-                    <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Contact</a>
+                    <div className='hidden md:block space-x-2'>
+                        <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Home</a>
+                        {/* <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">About Me</a> */}
+                        <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Resume</a>
+                        <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Projects</a>
+                        <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Contact</a>
+                    </div>
+                    <div className='block md:hidden'>
+                        {toggle ? (
+                            <AiOutlineClose onClick={closeMenu} size={30} 
+                            className='text-white'/>
+                        ):(<HiMenuAlt1 onClick={openMenu} size={30} 
+                        className='text-white'/>)}
+                    </div>
                 </div>
+            </div>
+
+            <div className='block md:hidden'>
+                {toggle ? (
+                    <div className='flex justify-between ml-10'>
+                        <ul>
+                            <li className='text-white hover:bg-indigo-800 text-xl mb-2 cursor-pointer'>Home</li>
+                            {/* <li className='text-white hover:bg-indigo-800 text-xl mb-2 cursor-pointer'>About Me</li> */}
+                            <li className='text-white hover:bg-indigo-800 text-xl mb-2 cursor-pointer'>Resume</li>
+                            <li className='text-white hover:bg-indigo-800 text-xl mb-2 cursor-pointer'>Projects</li>
+                            <li className='text-white hover:bg-indigo-800 text-xl mb-2 cursor-pointer'>Contact</li>
+                        </ul>
+                    </div>
+                    ):(
+                        <div></div>
+                )}
             </div>
         </>
     )
